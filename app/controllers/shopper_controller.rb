c0a0f19586ebc80bf8508ee2bc123b9d948d3ac2
Siyi Mei <siyimei98@gmail.com>
2022-12-08 09:42:15 -0800

    Add search

--- app/controllers/shopper_controller.rb
+++ app/controllers/shopper_controller.rb
@@ -1,8 +1,14 @@
 class ShopperController < ApplicationController
   include CurrentCart
   before_action :set_cart
+
   def index
-    @products=Product.order(:name)
+    @products=Product.all
+    @products=@products.where("name =?", params[:n]) if params[:n].present?
+    @products=@products.where("description LIKE ?", params[:d] + "%") if params[:d].present?
+    @products=@products.min_price(params[:min]) if params[:min].present?
+    @products=@products.min_price(params[:max]) if params[:max].present?
+
   end
 
   # GET /shopper/1 or /shopper/1.json
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
   validates(:image,format: {with: %r{\.(gif|jpg|png)\Z}i,message:"Must be gif,jpg or png images"})
   has_many :lineitems
   before_destroy :make_sure_this_product_is_not_a_lineitem
+  scope :min_price, ->(min) { where('price > ?', min) }
+  scope :max_price, ->(max) { where('price < ?', max) }
   def make_sure_this_product_is_not_a_lineitem
     if self.lineitems.empty?
       return true
